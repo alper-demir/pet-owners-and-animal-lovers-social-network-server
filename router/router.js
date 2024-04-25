b92bda7068d09540d7cc91921e151583d76b2a1d
Alper <alper.demir.fener@gmail.com>
2024-04-25 20:05:02 +0300

    added sending follow requests and private-public profile
    Follow requests can be send now.
    Users may accept and reject those requests.
    Private-public profile added.

--- router/router.js
+++ router/router.js
@@ -3,7 +3,7 @@ import User from "../models/User.js"
 import Post from "../models/Post.js"
 import Pet from "../models/PetProfile.js"
 import Comment from "../models/Comments.js"
-import Like from "../models/Likes.js"
+import FollowRequest from "../models/FollowRequest.js"
 import bcrypt from "bcrypt"
 import verifyToken from "../middlewares/verifyToken.js"
 import generateToken from "../utils/generateToken.js"
@@ -81,7 +81,7 @@ router.post("/create-post", async (req, res) => {
 router.post("/user-data/:username", verifyToken, async (req, res) => {
     const username = req.params.username;
     try {
-        const user = await User.findOne({ username }, { about: 1, firstName: 1, lastName: 1, posts: 1, pets: 1, username: 1 });
+        const user = await User.findOne({ username }, { about: 1, firstName: 1, lastName: 1, posts: 1, pets: 1, username: 1, profileUrl: 1 });
         if (user) {
             return res.json({ user });
         }
@@ -238,7 +238,7 @@ router.post("/check-post-like-status", verifyToken, async (req, res) => {
         // Kullanıcının likes alanında postId'yi içeren bir girişi var mı kontrol et
         const user = await User.findById(userId);
         const liked = user.likes.includes(postId);
-        
+
         res.json({ liked });
     } catch (error) {
         console.error("Check post like status error:", error);
@@ -270,6 +270,150 @@ router.post("/check-post-like-status", verifyToken, async (req, res) => {
 //     }
 // });
 
+router.post("/send-friend-request", verifyToken, async (req, res) => {
+    const { senderId, receiverId } = req.body;
+    console.log(senderId, receiverId);
+    try {
+        const sender = await User.findById(senderId);
+        const receiver = await User.findById(receiverId);
+        if (senderId == receiverId) {
+            return res.status(400).json({ message: "You cannot send a friend request to yourself" });
+        }
+        // If the sender is already following the receiver, unfollow
+        if (sender.followings.includes(receiverId)) {
+            sender.followings.pull(receiverId);
+            receiver.followers.pull(senderId)
+            await sender.save();
+            await receiver.save();
+            await FollowRequest.findOneAndDelete({ senderId, receiverId })
+            return res.status(200).json({ message: "Unfollowed successfully" });
+        }
+        // If the receiver user's account is not private, add them directly as a follower
+        if (receiver.privacy !== "private") {
+            sender.followings.push(receiverId);
+            receiver.followers.push(senderId);
+            await sender.save();
+            await receiver.save();
+            await FollowRequest.create({ senderId, receiverId, status: "accepted" });
+            return res.status(200).json({ message: "Followed successfully" });
+        } else {
+            const existingRequest = await FollowRequest.findOne({ senderId, receiverId });
+
+            if (existingRequest) {
+                // If a request with the same sender and receiver is found, remove it
+                await FollowRequest.findByIdAndDelete(existingRequest._id);
+                return res.status(200).json({ message: "A previous request has been withdrawn." });
+            } else {
+                await FollowRequest.create({ senderId, receiverId, status: "pending" });
+                return res.status(200).json({ message: "Friend request sent successfully" });
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.get('/follow-requests/:receiverId', async (req, res) => {
+    const receiverId = req.params.receiverId;
+
+    try {
+        // Find requests with receiverId and status "pending"
+        const followRequests = await FollowRequest.find({ receiverId: receiverId, status: "pending" }).populate({ path: "senderId", select: { username: 1, profileUrl: 1 } });
+
+        if (followRequests) {
+            res.json(followRequests);
+        } else {
+            res.status(404).json({ message: 'Bekleyen takip isteği bulunamadı.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Sunucu hatası' });
+    }
+});
+
+// checks status of request for single user
+router.post("/check-friend-request", verifyToken, async (req, res) => {
+
+    const { senderId, receiverUsername } = req.body;
+    try {
+        const receiver = await User.findOne({ username: receiverUsername }, { _id: 1 })
+        const request = await FollowRequest.findOne({ senderId, receiverId: receiver._id });
+
+        if (request) {
+            return res.status(200).json({ status: request.status });
+        } else {
+            return res.status(200).json({ status: "not_requested" });
+        }
+    } catch (error) {
+        console.error("Error checking friend request:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
+
+// checks profile visibility for private accounts
+router.post("/check-profile-visibility", async (req, res) => {
+    const { currentUserId, username } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if (user._id.toString() === currentUserId) {
+            // If the user is on their own profile, don't do any checks
+            return res.status(200).json({ message: "Own profile" });
+        }
+
+        const isFollowing = user.followers.includes(currentUserId);
+        const isPrivate = user.privacy === "private";
+        let visibility = false;
+        if (!isPrivate || isFollowing) {
+            visibility = true
+        }
+
+        return res.status(200).json({ isFollowing, isPrivate, visibility });
+    } catch (error) {
+        console.error("Error checking profile visibility:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.put("/accept-follow-request/:requestId", verifyToken, async (req, res) => {
+    const { requestId } = req.params;
+    try {
+        const request = await FollowRequest.findByIdAndUpdate(requestId, { status: "accepted" }, { new: true });
+        if (!request) {
+            return res.status(404).json({ message: "Follow request not found" });
+        }
+        const receiver = await User.findById(request.receiverId);
+        const sender = await User.findById(request.senderId);
+        receiver.followers.push(sender._id);
+        sender.followings.push(receiver._id);
+        await receiver.save();
+        await sender.save();
+        return res.status(200).json({ message: "Follow request accepted successfully" });
+    } catch (error) {
+        console.error("Error accepting follow request:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.put("/reject-follow-request/:requestId", verifyToken, async (req, res) => {
+    const { requestId } = req.params;
+    try {
+        const request = await FollowRequest.findOneAndDelete(requestId)
+        if (!request) {
+            return res.status(404).json({ message: "Follow request not found" });
+        }
+        return res.status(200).json({ message: "Follow request rejected successfully" });
+    } catch (error) {
+        console.error("Error rejecting follow request:", error);
+        return res.status(500).json({ message: "Server error" });
+    }
+});
 
 
 
